f39a3df079061a2ca88a2704c7a9ca31a0e87601
CT-1326 <smw7567@gmail.com>
2026-01-22 17:57:41 +0900

    update invoice data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,10 +12,11 @@ const FormSchema = z.object({
     customerId: z.string(),
     amount: z.coerce.number(),
     status: z.enum(['pending', 'paid']),
-    data: z.string(),
+    date: z.string(),
 });
 
-const CreateInvoice = FormSchema.omit({ id: true, data: true });
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(fromData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -33,5 +34,24 @@ export async function createInvoice(fromData: FormData) {
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
     
